5ca088500432c62f79b3f05b6bb1590d403eaee5
pujapurkar <pujapurkar20@gmail.com>
2024-10-13 13:36:17 +0530

    Push code

--- App.js
+++ App.js
@@ -1,34 +1,9 @@
-import { StyleSheet, Text, View } from 'react-native'
-<<<<<<< HEAD
-import React from 'react'
-<<<<<<< HEAD
-import HomeScreen from './src/Screen/HomeScreen'
-
-import IntroScreen from './src/Screen/IntroScreen'
-import MapScreen from './src/Purkar_Puja/Screens/MapScreen'
-import PasswordFieldScreen from './src/Purkar_Puja/Screens/PasswordFieldScreen'
-import StackNavigatorScreen from './src/Purkar_Puja/Screens/StackNavigatorScreen'
-=======
-=======
->>>>>>> 8a8c1b7a76216bae31fec6cd06c6cffba9434178
-
->>>>>>> 5ba7058aabc22f8e56339555612f6f01fbbf7025
-const App = () => {
-  return (
-<<<<<<< HEAD
-    
-   // <IntroScreen/>
-   //<MapScreen/>
-   //<PasswordFieldScreen/>
-   <StackNavigatorScreen/>
-=======
-    <View>
-      <Text>App</Text>
-    </View>
->>>>>>> 8a8c1b7a76216bae31fec6cd06c6cffba9434178
+import React from "react";
+import { Text } from "react-native";
+const App=()=>{
+  return(
+    <Text>App</Text>
   )
 }
 
 export default App;
-
-const styles = StyleSheet.create({})
