4ef9542118d4ab78ad679ed7a563f3e9607e7cf3
gayatriwagh2003 <waghgayatri3434@gmail.com>
2024-10-14 22:51:49 +0530

    pull code

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import FlatlistScreen from './src/Gayatri/FlatlistScreen';
 const App = () => {
     return (
         <View>
-            <Text>App</Text>
+            {/* <Text>App</Text> */}
+            <FlatlistScreen/>
         </View>
     )
 }
---
+++ src/Gayatri/FlatlistScreen.js
@@ -0,0 +1,11 @@
+import { View } from "react-native"
+import { Text } from "react-native"
+
+const FlatlistScreen = () =>{
+    return(
+        <View>
+            <Text>hiisss</Text>
+        </View>
+    )
+}
+export default FlatlistScreen
