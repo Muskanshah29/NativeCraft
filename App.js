6355b4af924806880a82fc8fd59f115460917205
gayatriwagh2003 <waghgayatri3434@gmail.com>
2024-10-15 22:26:09 +0530

    pull code

--- App.js
+++ App.js
@@ -1,23 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import FlatlistScreen from './src/Gayatri/FlatlistScreen';
-import BottomTabNavigatorScreen from './src/MayuriWagh/screen/BottomTabNavigatorScreen';
-import PropsScreen from './src/MayuriWagh/screen/PropsScreen';
-import DrawerNavigatorScreen from './src/Gayatri/DrawerNavigatorScreen';
 
 
 
 const App = () => {
     return (
         <View>
-            {/* <Text>App</Text> */}
-            {/* <FlatlistScreen/> */}
-            {/* <BottomTabNavigatorScreen/> */}
-            {/* <PropsScreen/> */}
-            <DrawerNavigatorScreen/>
-            
-            {/* <FlatlistScreen/> */}
-            {/* <Toastscreen/> */}
+            <Text>App</Text>
         </View>
     )
 }
