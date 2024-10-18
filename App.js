6d4518cd91af584cc436b72a6b0038162f323553
mayuriWagh2002 <mayuriwagh19072002@gmail.com>
2024-10-18 19:11:23 +0530

    push code

--- App.js
+++ App.js
@@ -1,9 +1,5 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import HomeScreen from './src/Sakshi_Dube/Screen/HomeScreen';
-import BottomTabNavigatorScreen from './src/MayuriWagh/screen/BottomTabNavigatorScreen';
-import PropsScreen from './src/MayuriWagh/screen/PropsScreen';
-import SectionListScreen from './src/MayuriWagh/screen/SectionListScreen';
 
 const App = () => {
   return (
