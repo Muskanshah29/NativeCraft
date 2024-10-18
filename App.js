5918e8f348a180abfc280deaa049d35871795e4d
dubesakshi-23 <sakshidube234@gmail.com>
2024-10-18 14:54:53 +0530

    push

--- App.js
+++ App.js
@@ -1,15 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
-<<<<<<< HEAD
 import React from 'react'
-=======
 
->>>>>>> 5ba7058aabc22f8e56339555612f6f01fbbf7025
 const App = () => {
   return (
     <View>
       <Text>App</Text>
     </View>
-  )
+ )
 }
 
 export default App;
