b6adcecf73b5c615e7a0e3c8014ba37d2ebe4001
pujapurkar <pujapurkar20@gmail.com>
2024-10-19 10:29:47 +0530

    pull code

--- App.js
+++ App.js
@@ -1,12 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-<<<<<<< HEAD
-import MapScreen from './src/Purkar_Puja/Screens/MapScreen'
-const App = () => {
-  return (
-    <MapScreen/>
-  )
-=======
+
 
 const App = () => {
   return (
@@ -15,9 +9,8 @@ const App = () => {
         App
       </Text>
     </View>
- )
->>>>>>> eece0123c4392c6992cf4f282583d5c0ce9e1819
+  )
 }
-export default App
+export default App;
 
 
