3def1edc55ddb6d2914575edabf42d4b1a4e5f6f
mayuriWagh2002 <mayuriwagh19072002@gmail.com>
2024-10-20 11:05:35 +0530

    Pull

--- App.js
+++ App.js
@@ -1,17 +1,15 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import MapScreen from './src/Purkar_Puja/Screens/MapScreen';
+import PasswordFieldScreen from './src/Purkar_Puja/Screens/PasswordFieldScreen';
+import StackNavigatorScreen from './src/Purkar_Puja/Screens/StackNavigatorScreen';
 
 const App = () => {
   return (
-    <View>
-      <Text>
-        App
-      </Text>
-    </View>
+    // <MapScreen/>
+    <PasswordFieldScreen/>
+    // <StackNavigatorScreen/>
  )
 }
 
 export default App;
-
-const styles = StyleSheet.create({})
-export default App
