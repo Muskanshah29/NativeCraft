130d8de2e8463b5c5afde7266cae1bacbc07aa42
dubesakshi-23 <sakshidube234@gmail.com>
2024-10-20 17:41:47 +0530

    pull

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import DrawerNavigation from './src/Screen/Navigation/DrawerNavigation';
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+  <DrawerNavigation/>    
  )
 }
 
