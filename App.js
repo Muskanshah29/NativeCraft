db9a8db84d81dd36cda9df955181e95c07591427
gayatriwagh2003 <waghgayatri3434@gmail.com>
2024-10-29 17:46:36 +0530

    pull code

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import DemoUseState from './src/Hooks/DemoUseState'
+import BottomTabNav from './src/Gayatri/BottomTabNav'
+import TabNavigatorScreen from './src/Gayatri/BottomTabNav'
 
 
 const App = () => {
   return (
     <View>
       {/* <Text>hii Gayatri</Text> */}
-     <DemoUseState/>
+     {/* <DemoUseState/> */}
+    <BottomTabNav/>
     </View>
   )
 }
--- src/Hooks/DemoUseState.js
+++ src/Hooks/DemoUseState.js
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { View, Text, TextInput, Button } from "react-native"
+import { View, Text, TextInput, Button, StyleSheet } from "react-native"
 
 
 
@@ -20,20 +20,61 @@ import { View, Text, TextInput, Button } from "react-native"
 // }
 // export default DemoUseState
 
-const DemoUseState = () => {
-    const [count , setCount] = useState(0)
+// 2nd example 
+// const DemoUseState = () => {
+//     const [count , setCount] = useState(0)
 
-    const Inc = ()=>{
-        setCount((prevCount)=>prevCount+1)
-    }
-    const Dec = ()=>{
-        setCount((prevCount)=>prevCount-1)
+//     const Inc = ()=>{
+//         setCount((prevCount)=>prevCount+1)
+//     }
+//     const Dec = ()=>{
+//         setCount((prevCount)=>prevCount-1)
+//     }
+//     return(
+//         <View style={{justifyContent:'center',alignItems:'center',paddingTop:150}}>
+//           <Button title="Increament" onPress={Inc}/>
+//           <Text>{count}</Text>
+//           <Button title="decreament" onPress={Dec}/>
+//         </View>
+//     )
+// }
+// export default DemoUseState
+
+
+// const DemoUseState = () =>{
+//     const  [obj , setObj] = useState({name:'Gayatri', age:22,city:'Nashik'})
+//     const upData = ()=>{
+//             setObj({name:'Mayuri',age:23,city:'Pune'})
+//     }
+//     return(
+//         <View style={styles.Container}> 
+//             <Button title="Submit" onPress={upData}/> 
+//             <Text>{obj.name}</Text>
+//             <Text>{obj.age}</Text>
+//             <Text>{obj.city}</Text>
+//         </View>
+//     )
+// }
+// export default DemoUseState
+
+// const styles = StyleSheet.create({
+//     Container :{
+//          justifyContent:'center',
+//          alignItems:'center',
+//          paddingTop:300
+//     }
+// })
+
+const DemoUseState = ()=>{
+
+    const [fruit,setFruit] = useState(['Apple','Banana'])
+    const FirutData = ()=>{
+        setFruit([...fruit,'orange','PineApple']) // ...fruit add a name
     }
     return(
-        <View style={{justifyContent:'center',alignItems:'center',paddingTop:150}}>
-          <Button title="Increament" onPress={Inc}/>
-          <Text>{count}</Text>
-          <Button title="decreament" onPress={Dec}/>
+        <View style={{justifyContent:'center',alignItems:'center',paddingTop:350}}>
+            <Button title="submit" onPress={FirutData}/>
+            <Text>{fruit.join(',')}</Text>
         </View>
     )
 }
